a9e9413cf6a6155b67017956bbc711512f7113f2
nihar <navadkunder@gmail.com>
2023-04-14 20:16:22 +0530

    version 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,112 +1,128 @@
-
 const addtabpath = "../components/addTab.html"
 const revtabpath = "../components/revtab.html"
+const decktabpath = "../components/decktab.html"
+const path = "http://localhost:3000"
 var currentcard;
 
-$(document).ready(function() {
+$(document).ready(async function() {
     //handle navigation
-    loadRevtab();
+    loadDecktab();
     $(addtab).click(loadAddtab)
     $(reviewtab).click(loadRevtab);
-
-
+    $(decktab).click(loadDecktab);
     //handle add card request
     $( "form" ).submit(function( event ) {
         event.preventDefault();
-        
-        console.log('submit clicked');
       });
-    console.log('object :>> ', $("#addcardbtn"));
-    getminboxcard();
-
 })
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.type === "setText") {
-        alert( message.data);
-    }
-});
+function custdate(box=0) {
+    var arr = [0,1,2,3,5,10,30,60,90];
+    var d = new Date(Date.now()+arr[box]*24*60*60*1000);
+    var date = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
+    return date;
+}
 
+//change date at which to read the card
 
-//functoins to handle navigation
+//function to add days to card
+
+//functions to check if the date has passed
+
+//Loading tabs
 function loadAddtab(){
     $(mainbody).load(addtabpath, addtabsetup);
+    $("div>#reviewtab").parent().css({'border-radius':'0 0 0px 10px'}).addClass('bg-secondarypurple');
+    $("div>#addtab").parent().css({'border-radius':'0 0 0px 0px'}).removeClass('bg-secondarypurple');
+    $("div>#decktab").parent().css({'border-radius':'0 0 10px 0px'}).addClass('bg-secondarypurple');
+    
 }
 function loadRevtab(){
+    $("div>#reviewtab").parent().css({'border-radius':'0 0 0px 0px'}).removeClass('bg-secondarypurple');
+    $("div>#addtab").parent().css({'border-radius':'0 0 10px 0px'}).addClass('bg-secondarypurple');
+    $("div>#decktab").parent().css({'border-radius':'0 0 0px 10px'}).addClass('bg-secondarypurple');
     $(mainbody).load(revtabpath, revtabsetup);
 }
-
-// attach listerner to button
+function loadDecktab(){
+    $("div>#reviewtab").parent().css({'border-radius':'0 0 10px 0px'}).addClass('bg-secondarypurple');
+    $("div>#addtab").parent().css({'border-radius':'0 0 0px 0px'}).addClass('bg-secondarypurple');
+    $("div>#decktab").parent().css({'border-radius':'0 0 0px 0px'}).removeClass('bg-secondarypurple');
+    $(mainbody).load(decktabpath, decktabsetup);
+}
+// Setup functions
 function addtabsetup(){
     $("#addcardbtn").click(addCard)
     $("form").submit(function(event){
         event.preventDefault();
     })
 }
+function decktabsetup(){
+    getlist();
+}
 function revtabsetup(){
     // getlist();
     $("#difftab").hide();
-
     if(cookieToArray()[0].length >1){
-        $("#cardFront").text(getminboxcard()[2]);
-        currentcard = getminboxcard();
+        showfront();
     }else{
         $("#cardFront").text("You dont have any card yet");
     }
     $(showback).click(showBack);
     $(backbtn).click(showfront).hide();
     $(".diffbtn").click(diffhandler);
-
     $("form").submit(function(event){
         event.preventDefault();
     })
 }
 
-function showBack(item){
+//review tab functions
+
+async function showfront(){
+    $("#difftab").hide();
+    $(backbtn).hide();
     if(cookieToArray()[0].length >1){
-        $("#cardFront").text(getminboxcard()[3]);
+        currentcard = await gettodaycookie();
+        if(currentcard.length==0){
+            $("#cardFront").text('Congrats!! You are done for today');
+            $("#showtab").hide(); 
+        }else{
+            $("#cardFront").text(currentcard['front']);        
+            $("#showtab").show();   
+        }
+    }else{
+        $("#cardFront").text("You dont have any card yet");        
+        $("#showtab").hide(); 
+    }
+}
+function showBack(){
+    if(cookieToArray()[0].length >1){
+        $("#cardFront").text(currentcard['back']);
         $("#difftab").show();
     }else{
         $("#cardFront").text("You dont have any card yet");
     }
     $(backbtn).show();
     $("#showtab").hide();
-    
-    
-}
-function showfront(){
-    if(cookieToArray()[0].length >1){
-        $("#cardFront").text(getminboxcard()[2]);
-        currentcard = getminboxcard();
-    }else{
-        $("#cardFront").text("You dont have any card yet");
-    }
-    $("#difftab").hide();
-    $("#showtab").show();
-    
-    $(backbtn).hide();
 }
 
 function diffhandler(){
     switch (this.id) {
         case "easy":
             //'1=1&front=Whats your name&back=Nihar&box=1'
-            var number = parseInt(currentcard[4])>8 ? 8 : parseInt(currentcard[4])
-            cookie = `${currentcard[0].trim()}=${currentcard[0].trim()}&front=${currentcard[2].trim()}&back=${currentcard[3].trim()}&box=${parseInt(currentcard[4])+1}`
-            document.cookie = cookie;
+            var box = parseInt(currentcard['box'])>7 ? 8 : parseInt(currentcard['box'])+1;
+            updateCard(currentcard['id'], currentcard['front'], currentcard['back'], box, custdate(box));
             showfront();
             break;
-          
+            
         case "notsure":
-            cookie = `${currentcard[0].trim()}=${currentcard[0].trim()}&front=${currentcard[2].trim()}&back=${currentcard[3].trim()}&box=${parseInt(currentcard[4])}`
-            document.cookie = cookie;
+            var box =parseInt(currentcard['box']);
+            updateCard(currentcard['id'], currentcard['front'], currentcard['back'], box, custdate(box));
             showfront();           
             break;
-      
+            
         case "hard":
-            var number = parseInt(currentcard[4])<4 ? 1 : parseInt(currentcard[4])-3;
-            cookie = `${currentcard[0].trim()}=${currentcard[0].trim()}&front=${currentcard[2].trim()}&back=${currentcard[3].trim()}&box=${number}`
-            document.cookie = cookie;
+            var box = parseInt(currentcard['box'])<3 ? 0 : parseInt(currentcard['box'])-3;
+            updateCard(currentcard['id'], currentcard['front'], currentcard['back'], box, custdate(box));
             showfront();            
             break;
 
@@ -114,66 +130,99 @@ function diffhandler(){
             break;
     }
 }
+function updateCard(id, front, back, box, date){
+    axios.post(path+'/updatecard',{
+        id:id,
+        front:front,
+        back:back,
+        box:box,
+        revdate:date
+    }).then(function(response){
+        console.log('response :>> ', response);
+    })
+}
+async function gettodaycookie(){
+    var response = await axios.post(path+'/gettodaycookie',{today: custdate()})
+    currentcard = response.data;
+    return response.data; 
+}
+function getminboxcard(){
+    var cookie =cookieToArray(); 
+    var todaycookie=[];
+    cookie.forEach(function(c){
+        if(c[5] == custdate()){
+            todaycookie.push(c);
+        }        
+    })
+    if(todaycookie.length>0){
 
-//handle add card:
+        for (let box = 0; box <11; box++) {
+            for (let index = 0; index < todaycookie.length; index++) {
+                var cardbox = todaycookie[index][4];
+                if(cardbox == box){
+                    return todaycookie[index];
+                }
+            }        
+        }   
+    }else{
+        return [];
+    }
+}
+//Add tab functions:
 function addCard(){
     var id = getnewid()!==NaN ? getnewid(): 1;
     var front = $("#front").val().trim();
     var back = $("#back").val().trim();
-    var box = 1;
-    document.cookie = `${id}=${id}&front=${front}&back=${back}&box=${box}`;
-    // alert(document.cookie);
+    var box = 0;
+    axios.post(path+'/addcard',{
+        // id: id,
+        front: front,
+        back: back,
+        box: box,
+        date: custdate()
+    })
+        .then(function (response) {
+            
+        })
+    $("textarea").val("");  
     // every cookie represents a card
     //card has front, back, box and id
     //algorithm for flashcard: https://www.skoumal.com/en/how-does-the-learning-algorithm-in-the-flashcard-app-vocabulary-miner-work/
 }
-
-
-
-
-function getnewid(){
-    var cookielist = document.cookie.split(";");
-    var idlist = cookielist.map(function(c){
-        parameters = c.split("&");
-
-        id = parseInt(parameters[0].split("=")[0].trim());
-        return id;
-    })
-    var arraymax = Math.max.apply(Math, idlist);
-    var newid = arraymax>0? arraymax+1 : 1;
-    return newid;
-}
-
-function getminboxcard(){
-    var cookie =cookieToArray();
-    for (let box = 0; box < 9; box++) {
-        for (let index = 0; index < cookie.length; index++) {
-            var cardbox = cookie[index][4];
-            if(cardbox == box){
-                return cookie[index];
-            }
-        }        
-    }
-}
+//Deck tab Functions
 //function for getting list of cards:
 function getlist(){
-    $("#list").empty();
-    var cookielist = document.cookie.split(";");
-    cookielist.forEach(function(cookie){
-        params = cookie.split("&");
-        valuelist = params.map(function(item){
-            keyval = item.split("=");
-            return keyval[1];
+    var cookielist;
+    var imgsize = 34;
+    axios.post(path+'/getallcard')
+    .then(function(response){
+            cookielist = response.data;
+            $("#list").empty();
+            $("#list").append(`<tr>
+            <th>Front</th>
+                    <th>Back</th>
+                    <th>Rev on</th>
+                    <th>Del</th>
+                    </tr>`)
+            cookielist.forEach(function(cookie){
+                $("#list").append(` <tr>
+                    <td>${cookie.front}</td>
+                    <td>${cookie.back}</td>
+                    <td>${cookie.revdate}</td>
+                    <td><img id="${cookie.id}" class="delbtn" width="${imgsize}" height="${imgsize}" src="../Assets/delete_icon.png"/></td>
+                    </tr>`)
+                });
+            $(".delbtn").click(function(e){
+                del_cookie(e.target.id);
+            })
+        });
+}
+function del_cookie(id){
+    axios.post(path+'/deletecard', {id:id})
+    .then(function(response){
+            console.log('delete response :>> ', response);
         })
-        if(valuelist.length>0){
-            $("#list").append(` <tr>
-            <td>${valuelist[0]}</td>
-            <td>${valuelist[1]}</td>
-            <td>${valuelist[2]}</td>
-            <td>${valuelist[3]}</td>
-            </tr>`)
-        }
-    });
+    getlist();
 }
 function get_cookie(name){
     return document.cookie.split(';').some(c => {
@@ -181,21 +230,8 @@ function get_cookie(name){
     });
 }
 
-function delete_cookie( name, path, domain ) {
-    if( get_cookie( name ) ) {
-      document.cookie = name + "=" +
-        ((path) ? ";path="+path:"")+
-        ((domain)?";domain="+domain:"") +
-        ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-    }
-}
 
-function cookieToArray(){
-    var cookielist = document.cookie;
-    var myjson = '[["'+cookielist.replaceAll('&', '" , "').replaceAll('front=',' ').replaceAll('back=',' ').replaceAll(';','" ], [ "').replaceAll('box=',' ').replaceAll('=','", "')+ '"]]';
-    var array = JSON.parse(myjson);
-    return array;
-}
+//converts cookie data into array format
 
 // 1=1&front=fjdsoifj&back=jfoidj&box=1; 2=2&front=hellwo&back=iodj&box=1; 3=3&front=difjo&back=jfio&box=1; 4=4&front=idsjof&back=ijfod&box=1
 
@@ -218,3 +254,41 @@ function cookieToArray(){
 
 
 
+//Legacy code:
+// function addCard(){
+//     var id = getnewid()!==NaN ? getnewid(): 1;
+//     var front = $("#front").val().trim();
+//     var back = $("#back").val().trim();
+//     var box = 0;
+//     document.cookie = `${id}=${id}&front=${front}&back=${back}&box=${box}&date=${custdate()}`;
+//     $("textarea").val("");
+//     // alert(document.cookie);
+//     // every cookie represents a card
+//     //card has front, back, box and id
+//     //algorithm for flashcard: https://www.skoumal.com/en/how-does-the-learning-algorithm-in-the-flashcard-app-vocabulary-miner-work/
+// }
+function getnewid(){
+    var cookielist = document.cookie.split(";");
+    var idlist = cookielist.map(function(c){
+        parameters = c.split("&");
+        id = parseInt(parameters[0].split("=")[0].trim());
+        return id;
+    })
+    var arraymax = Math.max.apply(Math, idlist);
+    var newid = arraymax>0? arraymax+1 : 1;
+    return newid;
+}
+function cookieToArray(){
+    var cookielist = document.cookie;
+    var myjson = '[["'+cookielist.replaceAll('&', '" , "').replaceAll('front=','').replaceAll('back=','').replaceAll(';','" ], [ "').replaceAll('box=','').replaceAll('date=','').replaceAll('=','", "')+ '"]]';
+    var array = JSON.parse(myjson);
+    return array;
+}
+function delete_cookie( name, path, domain ) {
+    if( get_cookie( name ) ) {
+      document.cookie = name + "=" +
+      ((path) ? ";path="+path:"")+
+        ((domain)?";domain="+domain:"") +
+        ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    }
+}
